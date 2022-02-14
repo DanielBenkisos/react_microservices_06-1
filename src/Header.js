7c433712ac3228632c658e71bd6ef7581ab07b54
mahnac <jamessejercanham@gmail.com>
2022-02-14 09:57:49 +0100

    Create simple header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>About</div>
+            <a href="#">Login</a>
+            <a href="#">Sign Up</a>
+            <a href="#">Logout</a>
+        </div>
+    );
+};
+
+export default Header;
