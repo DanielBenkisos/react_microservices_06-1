f7a41f2d16f16f302ce74be54000e40966e8d51b
mahnac <jamessejercanham@gmail.com>
2022-02-16 12:06:48 +0100

    create pages folder and delete shit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from "./components/Login";
-import SignUp from "./components/Signup";
-import Store from "./components/Store";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import Store from "./pages/Store";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -30,8 +29,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
